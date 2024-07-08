c7efdaf2b6b04bbfd92228ad74495883cf795533
Liu Yu <liuyu@comp.nus.edu.sg>
2024-07-08 14:43:02 +0800

    lv1

---
+++ benchmark/protocols/lv1.cpp
@@ -0,0 +1,25 @@
+/*
+Patch: https://github.com/CTL-Datalog/Dataset/blob/main/lv1/patch.diff#L106-L109
+*/
+
+# include <iostream>
+
+class RTSPClientConnection {
+  
+};
+
+void handleHTTPCmd_notSupported() {
+  
+}
+
+int main(){
+  RTSPClientConnection *prevClientConnection;
+  bool fIsActive;
+  
+  if (prevClientConnection == NULL) {
+    // There was no previous HTTP "GET" request; treat this "POST" request as bad:
+    handleHTTPCmd_notSupported();
+    fIsActive = false; // triggers deletion of ourself
+    return false;
+  }
+}
